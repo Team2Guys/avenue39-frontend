0b41ae93ac948fbd39d1998302e60a9c1c96ab9c
misbahkhalil07 <misbahperveenpaf@gmail.com>
2025-11-19 13:30:26 +0500

    hello

--- src/components/Common/CommonButton.tsx
+++ src/components/Common/CommonButton.tsx
@@ -12,7 +12,7 @@ export const Button = ({
   if (href) {
     return (
       <Link href={href} className={`text-center p-2 rounded-md ${className}`}>
-        {text}
+        {text}1
       </Link>
     );
   }
